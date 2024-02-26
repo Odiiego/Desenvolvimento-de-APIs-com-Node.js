48caca3afcc1ea93bc59adfdae9ae6f7fe215e5b
Odiiego <odiegopaula@gmail.com>
2024-02-26 04:52:30 -0300

    chore: Started the test environment with an in-memory database

---
+++ test.js
@@ -0,0 +1,21 @@
+const { deepEqual, ok } = require('assert');
+const database = require('./database');
+
+const DEFAULT_ITEM_CADASTRAR = {
+  nome: 'Flash',
+  poder: 'Speed',
+  id: 1,
+};
+
+describe('Suite de manipulação de Heróis', () => {
+  it('Deve pesquisar um herói usando arquivos', async () => {
+    const expected = DEFAULT_ITEM_CADASTRAR;
+    const [result] = await database.listar(expected.id);
+    deepEqual(result, expected);
+  });
+  // it('Deve cadastrar um herói, usando arquivos', async () => {
+  //   const expected = {};
+
+  //   ok(null, expected);
+  // });
+});
