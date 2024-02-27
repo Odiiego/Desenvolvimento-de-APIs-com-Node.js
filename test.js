cb543e44d302efe3b34a14de3c4f8a12c77f8f6f
Odiiego <odiegopaula@gmail.com>
2024-02-26 21:12:24 -0300

    test: User deletion test

--- test.js
+++ test.js
@@ -8,16 +8,21 @@ const DEFAULT_ITEM_CADASTRAR = {
 };
 
 describe('Suite de manipulação de Heróis', () => {
-  it('Deve pesquisar um herói usando arquivos', async () => {
-    const expected = DEFAULT_ITEM_CADASTRAR;
-    const [result] = await database.listar(expected.id);
-    deepEqual(result, expected);
-  });
   it('Deve cadastrar um herói, usando arquivos', async () => {
     const expected = DEFAULT_ITEM_CADASTRAR;
     await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
     const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id);
 
     deepEqual(actual, expected);
+  });
+  it('Deve pesquisar um herói usando arquivos', async () => {
+    const expected = DEFAULT_ITEM_CADASTRAR;
+    const [result] = await database.listar(expected.id);
+    deepEqual(result, expected);
+  });
+  it('Deve remover um herói por id', async () => {
+    const expected = true;
+    const result = await database.remover(DEFAULT_ITEM_CADASTRAR.id);
+    deepEqual(result, expected);
   });
 });
